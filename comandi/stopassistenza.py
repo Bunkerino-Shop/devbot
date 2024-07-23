d540e88d9d8302349e62da75a16ebce9a6e576fe
Luca Cassano <lcassano2007+account@gmail.com>
2024-07-23 17:49:28 +0200

    Update

--- comandi/stopassistenza.py
+++ comandi/stopassistenza.py
@@ -1,10 +1,10 @@
 from variabili import SUPPORT_GROUP_ID, assistenza_sessions
+print('Caricato comando stopassistenza')
 def register(bot):
     @bot.message_handler(commands=['stopassistenza'])
     def handle_stop_assistenza(message):
         user_id = message.chat.id
         if user_id in assistenza_sessions:
             assistenza_sessions[user_id]['active'] = False
-            bot.reply_to(message, 'La tua sessione di assistenza è stata terminata. Grazie per averci contattato!')
-            # Notifica il gruppo di supporto
-            bot.send_message(SUPPORT_GROUP_ID, f"L'utente {user_id} ha terminato la sessione di assistenza.")
+            bot.send_message(user_id, 'La tua sessione di assistenza è stata terminata. Grazie per averci contattato!')
+            bot.send_message(SUPPORT_GROUP_ID, f"La sessione di assistenza dell'utente {user_id} è stata terminata dall'utente.")
--- start.py
+++ start.py
@@ -13,6 +13,7 @@ bot = telebot.TeleBot(TOKEN)
 
 # Carica dinamicamente i comandi dalla cartella 'comandi'
 def load_commands(bot):
+    print('Caricamento comandi...')
     commands_path = 'comandi'
     for filename in os.listdir(commands_path):
         if filename.endswith('.py'):
@@ -22,27 +23,6 @@ def load_commands(bot):
             spec.loader.exec_module(module)
             print(dir(module))
             module.register(bot)
-
-# Gestisci il comando /staffstopassistenza (staff)
-@bot.message_handler(commands=['staffstopassistenza'])
-def handle_staff_stop_assistenza(message):
-    if message.reply_to_message and message.reply_to_message.forward_from:
-        user_id = message.reply_to_message.forward_from.id
-        if user_id in assistenza_sessions:
-            assistenza_sessions[user_id]['active'] = False
-            bot.send_message(message.chat.id, f"La sessione di assistenza dell'utente {user_id} è stata terminata dallo staff.")
-            bot.send_message(user_id, 'La tua sessione di assistenza è stata terminata dallo staff. Grazie per averci contattato!')
-
-# Gestisci il comando /assistenza
-@bot.message_handler(commands=['assistenza'])
-def handle_assistenza(message):
-    user_id = message.chat.id
-    assistenza_sessions[user_id] = {
-        'user_chat_id': user_id,
-        'active': True,
-        'last_forwarded_message_id': None
-    }
-    bot.reply_to(message, 'Sei in contatto con il supporto. Invia i tuoi messaggi e ti risponderemo al più presto.')
 
 # Gestisci i messaggi degli utenti e inoltrali al gruppo di supporto
 @bot.message_handler(func=lambda message: message.chat.id in assistenza_sessions and assistenza_sessions[message.chat.id]['active'])
